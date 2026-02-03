53c19e01b17784d3be0a86aa22d809f894a0fb2e
kiwi <kiwi@royalwood2008.com>
2026-02-03 08:09:52 +0800

    fix: add favicon to public and configure strapi favicon

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,6 +7,11 @@ export default [
   'strapi::query',
   'strapi::body',
   'strapi::session',
-  'strapi::favicon',
+  {
+    name: 'strapi::favicon',
+    config: {
+      path: './public/favicon.ico',
+    },
+  },
   'strapi::public',
 ];
